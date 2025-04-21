2c0a0cc30e6d5619ac60110ccd6736826a387ce0
ComputerCorgi <marco@premoli.net>
2025-04-21 14:29:18 +0200

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import WebApp from '@twa-dev/sdk'
 
-WebApp.ready()
+WebApp.ready();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
