35a79287261dada86e9c41cbe8d143ecdf59027f
ComputerCorgi <marco@premoli.net>
2025-04-21 21:02:14 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import WebApp from '@twa-dev/sdk'
 
 function App() {
 
+  {document.documentElement.className = WebApp.colorScheme}
+
   return (
     <>
-      <div>
-        {document.body.style.colorScheme}
-        {WebApp.colorScheme}
+      <div >
+        {document.documentElement.className = WebApp.colorScheme}
       </div>
     </>
   )
