34c0866d95aaa163a545078ac06a02574d6447b9
ComputerCorgi <marco@premoli.net>
2025-04-21 13:49:35 +0200

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -7,7 +7,7 @@ import WebApp from '@twa-dev/sdk'
 WebApp.ready()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>,
 )
