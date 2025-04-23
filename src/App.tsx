034393218bde387f9befa9e432ad7296a4f352a1
ComputerCorgi <marco@premoli.net>
2025-04-23 07:52:56 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,20 @@ function createFoodIteam(iteam: Food) {
 
 function App() {
 
-  const foodList: Food[] = [{img_url: 'url', name: 'name', price: 0, quantity: 0}]
+  const foodList: Food[] = [
+    {img_url: './public/Burger_148.png', name: 'Burger', price: 4.99, quantity: 0},
+    {img_url: './public/Fries_148.png', name: 'Fries', price: 1.49, quantity: 0},
+    {img_url: './public/Hotdog_148.png', name: 'Hotdog', price: 3.49, quantity: 0},
+    {img_url: './public/Tako_148.png', name: 'Taco', price: 3.99, quantity: 0},
+    {img_url: './public/Pizza_148.png', name: 'Pizza', price: 7.99, quantity: 0},
+    {img_url: './public/Donut_148.png', name: 'Donut', price: 1.49, quantity: 0},
+    {img_url: './public/Popcorn_148.png', name: 'Popcorn', price: 1.99, quantity: 0},
+    {img_url: './public/Coke_148.png', name: 'Coke', price: 1.49, quantity: 0},
+    {img_url: './public/Icecream_148.png', name: 'Icecream', price: 5.99, quantity: 0},
+    {img_url: './public/Cookie_148.png', name: 'Cookie', price: 3.99, quantity: 0},
+    {img_url: './public/Flan_148.png', name: 'Flan', price: 7.99, quantity: 0},
+    {img_url: './public/Cake_148.png', name: 'Cake', price: 9.99, quantity: 0},
+]
 
   return (
     <>
